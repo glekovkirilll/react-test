35aacb89e9bcfe3e1b1c8c86877ef14b6a063c35
glekovkirilll <glekovkirill2016@gmail.com>
2022-10-11 21:32:43 +0700

    add smth

--- src/Tree/Tree.js
+++ src/Tree/Tree.js
@@ -5,6 +5,23 @@ export default function Tree(){
     const [treeChildren, setChildren] = useState([]);
     const [newId, setNewId] = useState(0);
 
+    const [selectedItem, setSelectedItem] = useState({})
+
+    const selectItem = useCallback((addFunc, removeFunc, editFunc, selectFunc) => {
+        if (Object.keys(selectedItem).length !== 0) {
+            selectedItem.onSelectChild(false)
+        }
+
+        setSelectedItem({
+            onAddChild: addFunc,
+            onRemoveChild: removeFunc,
+            onEditChild: editFunc,
+            onSelectChild: selectFunc
+        })
+
+        selectFunc(true)
+    }, [selectedItem])
+
     const deleteChild = useCallback(id=>{
         const newChildren = treeChildren.filter(elemnt => elemnt.id !== id)
         setChildren(newChildren)
@@ -23,7 +40,18 @@ export default function Tree(){
                 <button onClick={addChild}>Add</button>
                 <button onClick={() => setChildren([])}>Reset</button>
             </div>
-            {treeChildren.map(child => (<TreeItem key={child.id} deleteSelf={() => deleteChild(child.id)} parentId={child.id} />))}
+            {treeChildren.map(child => (
+                <TreeItem 
+                    key={child.id} 
+                    deleteSelf={() => deleteChild(child.id)} 
+                    parentId={child.id} 
+                    selectItem={selectItem}/>
+                ))    
+            }
+            <div style={{marginTop: '20px'}}>
+                <button onClick={selectedItem.onAddChild}>Add</button>
+                <button onClick={selectedItem.onRemoveChild}>Remove</button>
+            </div>
         </div>
     )
 }
--- src/Tree/TreeItem.js
+++ src/Tree/TreeItem.js
@@ -1,42 +1,57 @@
 import React, { useCallback, useState } from "react";
 
-export default function TreeItem({deleteSelf, parentId}){
+export default function TreeItem({deleteSelf, parentId, selectItem}){
     const [treeChildren, setChildren] = useState([]);
     const [treeName, setName] = useState('New item');
     const [isShowCildren, setIsShowChildren] = useState(true);
     const [isEditingName, setEditingName] = useState(false);
     const [newId, setNewId] = useState(0);
+    const [isSelected, setIsSelected] = useState(false)
 
     const deleteChild = useCallback(id=>{
         const newChildren = treeChildren.filter(elemnt => elemnt.id !== id)
         setChildren(newChildren)
     }, [treeChildren])
 
-    const addChild = () => {
+    const addChild = useCallback(() => {
         const id = newId;
         setNewId(newId + 1)
         let newChild={id: parentId + '_' + id }
         setChildren([...treeChildren, newChild])
+    },[])
+
+    const handleSelect = () => {
+        // setIsSelected(true)
+        selectItem(() => addChild(), deleteSelf, setEditingName, setIsSelected)
     }
 
     return(
         <div style={{ paddingLeft: "10px"}}>
-            { isEditingName ? 
-                <input type="text" value={treeName} onChange={(e)=>setName(e.target.value)}></input> :                
-                <span className="Name" onClick={() => setIsShowChildren(!isShowCildren)}>{treeName}</span>
-            }
+            <span className="Name" style={{backgroundColor: isSelected ? '#F08080' : 'transparent'}} onClick={() => handleSelect(true)}>{treeName}</span>
 
-            {
+            {/* {
                 isEditingName?
                     <button onClick={() => setEditingName(!isEditingName)}>Apply </button>: 
                     <button onClick={() => setEditingName(!isEditingName)}>Edit </button>
+            } */}
+
+            {isShowCildren ?
+                <button onClick={() => setIsShowChildren(!isShowCildren)}>Hide </button>: 
+                <button onClick={() => setIsShowChildren(!isShowCildren)}>Show </button>
             }
+
 
             <button onClick={addChild}>Add</button>
             
             <button onClick={deleteSelf}>Delete</button>
             {/* {isShowCildren ? treeChildren.map(child => child.element) : null} */}
-            {isShowCildren ? treeChildren.map(child => (<TreeItem key={child.id} deleteSelf={() => deleteChild(child.id)} parentId={child.id} />)) : null}
+            {isShowCildren ? treeChildren.map(child => (
+                <TreeItem  
+                    key={child.id} 
+                    deleteSelf={() => deleteChild(child.id)} 
+                    parentId={child.id} 
+                    selectItem={selectItem} />
+                )) : null}
         </div>
     )
 }
